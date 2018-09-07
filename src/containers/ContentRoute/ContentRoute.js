cf45c951a534b07ae1e0271832df904171880cf8
sojurner <pykim0591@gmail.com>
2018-09-07 07:23:44 -0600

    Add MDTP to contentRoute

--- src/containers/ContentRoute/ContentRoute.js
+++ src/containers/ContentRoute/ContentRoute.js
@@ -23,8 +23,6 @@ export default ContentRoute;
 // 'https://images.unsplash.com/38/L2NfDz5SOm7Gbf755qpw_DSCF0490.jpg?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjI0MX0&s=717f905b696bd33ced31d380b1283b60'
 // 'https://images.unsplash.com/photo-1519864967-22b37f31a770?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjI0MX0&s=307c453c1e85edef8468f925da9e1f74'
 
-// const mapStateToProps = state => ({
-//   currentUser
-// });
+const mapStateToProps = state => ({});
 
 // export default connect;
