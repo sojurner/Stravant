5fe5d5a4bff8d83469e697aa2484de68ef290bab
sojurner <pykim0591@gmail.com>
2018-09-08 19:36:32 -0600

    Create recursive function to fetch weekly data of seven data sets divided into 24 hour increments

--- src/helpers/apiCalls/apiCalls.js
+++ src/helpers/apiCalls/apiCalls.js
@@ -28,6 +28,21 @@ export const getUserStats = async (token, num) => {
   const result = await Promise.all(weeklyStats);
   return scrape.weeklyData(result);
 };
+
+const recursiveRetrival = async (token, num) => {
+  const before = Math.floor(Date.now() / 1000 - 86400 * num);
+  const after = Math.floor(Date.now() / 1000 - 86400 - 86400 * num);
+  const url = `https://www.strava.com/api/v3/athlete/activities?before=${before}&after=${after}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  };
+  const response = await fetch(url, options);
+  return await response.json();
+};
   const response = await fetch(url, options);
   return await response.json();
 };
