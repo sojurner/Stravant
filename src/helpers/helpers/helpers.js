9b81e99e695d40017342cee937fcc5e3876fc4e6
sojurner <pykim0591@gmail.com>
2018-09-08 19:45:06 -0600

    This was by far the most confusing scrape I have ever done.  Something that i thought would be relatively easy ended up taking an hour to figure out

--- src/helpers/helpers/helpers.js
+++ src/helpers/helpers/helpers.js
@@ -12,3 +12,38 @@ export const userInfo = info => {
 export const userDailyStats = stats => {
   console.log(stats);
 };
+
+export const clubData = (info, activities) => {
+  const matchingClub = info.find(club => club.name === 'Stravant Club');
+  const mappedActivities = activities.reduce((userActivity, activity) => {
+    if (!userActivity[activity.athlete.firstname]) {
+      userActivity[activity.athlete.firstname] = {
+        totalDistance: 0,
+        totalTime: 0,
+        membersRecent: '',
+        mostRecent: {
+          distance: activities[0].distance / 1609,
+          activity: activities[0].name
+        }
+      };
+    }
+    const distance = activity.distance / 1609;
+    let preciseDistance = Math.round(distance * 100) / 100;
+
+    userActivity[activity.athlete.firstname].totalDistance += preciseDistance;
+
+    userActivity[activity.athlete.firstname].totalTime += Math.floor(
+      activity.elapsed_time / 60
+    );
+
+    userActivity[activity.athlete.firstname].membersRecent = activity.name;
+    return userActivity;
+  }, {});
+
+  const clubInfo = {
+    clubName: matchingClub.name,
+    clubId: matchingClub.id,
+    membersStats: mappedActivities
+  };
+  return clubInfo;
+};
