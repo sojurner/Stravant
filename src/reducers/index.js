a1eaa9911931d10e5147ee12364f27bd6725be9f
sojurner <pykim0591@gmail.com>
2018-09-08 19:48:30 -0600

    Add clubs to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { userReducer } from './userReducer';
+import { clubReducer } from './clubReducer';
 
 const rootReducer = combineReducers({
-  currentUser: userReducer
+  currentUser: userReducer,
+  clubs: clubReducer
 });
 
 export default rootReducer;
