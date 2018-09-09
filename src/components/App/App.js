c4ccb3f2557b6513e5705f4607202a27100c5363
sojurner <pykim0591@gmail.com>
2018-09-08 19:28:19 -0600

    Add conditional rendering to nav bar, content route and welcome page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import ContentRoute from '../../containers/ContentRoute/ContentRoute';
 import { NavBar } from '../../containers/NavBar/NavBar';
 import Welcome from '../../containers/Welcome/Welcome';
-import { stravaApi } from '../../data/strava_config';
 
-import CompareAll from '../../containers/CompareAll/CompareAll';
-const strava = require('strava-v3');
+// const strava = require('strava-v3');
 
 export class App extends Component {
   constructor() {
@@ -16,19 +16,27 @@ export class App extends Component {
   }
 
   render() {
+    const { currentUser } = this.props;
+
     return (
-      <div className="App">
-        )<Welcome />
-        <NavBar />
-        <ContentRoute />
-        <img
-          className="avatar"
-          src="https://boutique.alforme.fr/wp-content/uploads/2017/08/avatar-homme.png"
-          height="100"
-        />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          {currentUser && <Welcome />}
+          {currentUser && <NavBar />}
+          {currentUser && <ContentRoute />}
+          <img
+            className="avatar"
+            src="https://boutique.alforme.fr/wp-content/uploads/2017/08/avatar-homme.png"
+            height="100"
+          />
+        </div>
+      </BrowserRouter>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  currentUser: state.currentUser.info
+});
+
+export default connect(mapStateToProps)(App);
