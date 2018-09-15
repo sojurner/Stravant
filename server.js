d6161ea5c6d4c0432966ecb4ae4befaaba876f1b
sojurner <pykim0591@gmail.com>
2018-09-14 22:21:16 -0600

    Import socket, new instance of socket

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require('express');
-var socketIo = require('socket.io');
+var socket = require('socket.io');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+var server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'your express is connected to react' });
+});
+
+var io = socket(server);
+
+io.on('connection', socket => {
+  socket.on('message', function(msg) {
+    console.log('message:' + msg);
+  });
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
 });
