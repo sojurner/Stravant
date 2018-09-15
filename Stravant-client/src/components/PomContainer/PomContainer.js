18648d7741433887bcaeb04482cf0757e805b653
sojurner <pykim0591@gmail.com>
2018-09-14 22:24:44 -0600

    Connect to store mstp, mdtp

--- Stravant-client/src/components/PomContainer/PomContainer.js
+++ Stravant-client/src/components/PomContainer/PomContainer.js
@@ -24,4 +24,18 @@ export class PomContainer extends Component {
     );
   });
   return <div className="pom-container">{pomContainer}</div>;
-};
+  }
+}
+
+const mapStateToProps = state => ({
+  pomHistory: state.pomInfo.pomHistory
+});
+
+const mapDispatchToProps = dispatch => ({
+  removePom: (history, time) => dispatch(removePom(history, time))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PomContainer);
