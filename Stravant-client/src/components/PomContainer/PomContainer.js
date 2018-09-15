251571c63cb4992a9bce4efc79fbe3fa12cc6a9c
sojurner <pykim0591@gmail.com>
2018-09-14 22:25:01 -0600

    Pom container

--- Stravant-client/src/components/PomContainer/PomContainer.js
+++ Stravant-client/src/components/PomContainer/PomContainer.js
@@ -8,22 +8,22 @@ export class PomContainer extends Component {
   render() {
     const { pomHistory, removePom } = this.props;
 
-  const pomContainer = Object.keys(pomHistory).map(time => {
-    const pomTime = `${pomHistory[time].second}sec ${
-      pomHistory[time].minute
-    }min ${pomHistory[time].hour}hour`;
-    return (
-      <div className="pom-cards">
-        <i
-          class="fas fa-trash-alt"
-          onClick={() => removePom(pomHistory, time)}
-        />
-        <h4>{time} </h4>
-        <p>{pomTime}</p>
-      </div>
-    );
-  });
-  return <div className="pom-container">{pomContainer}</div>;
+    const pomContainer = Object.keys(pomHistory).map(time => {
+      const pomTime = `${pomHistory[time].second}sec ${
+        pomHistory[time].minute
+      }min ${pomHistory[time].hour}hour`;
+      return (
+        <div className="pom-cards">
+          <i
+            class="fas fa-trash-alt"
+            onClick={() => removePom(pomHistory, time)}
+          />
+          <h4>{time} </h4>
+          <p>{pomTime}</p>
+        </div>
+      );
+    });
+    return <div className="pom-container">{pomContainer}</div>;
   }
 }
 
