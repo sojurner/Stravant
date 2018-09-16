ee493b6357d80b1c1729404d5c459735c212d79c
sojurner <pykim0591@gmail.com>
2018-09-16 09:56:31 -0600

    Add listener from client

--- server.js
+++ server.js
@@ -6,16 +6,23 @@ const port = process.env.PORT || 5000;
 
 var server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'your express is connected to react' });
-});
+// app.get('/express_backend', (req, res) => {
+//   res.send({ express: 'your express is connected to react' });
+// });
 
 var io = socket(server);
 
 io.on('connection', socket => {
-  socket.on('message', function(msg) {
-    console.log('message:' + msg);
+  console.log('user connected');
+  socket.on('pomStart', function(msg) {
+    io.emit('pomClick', msg);
+    console.log(msg);
   });
+  socket.on('pomEnd', function(msg) {
+    io.emit('pomClick', msg);
+    console.log(msg);
+  });
+
   socket.on('disconnect', function() {
     console.log('user disconnected');
   });
